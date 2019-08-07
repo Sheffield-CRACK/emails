2b048ddfe30cab01e918ac8260eebbd82f30bd82
James Bruten <jrbruten1@sheffield.ac.uk>
2019-08-07 11:13:01 +0100

    Update unsubsribe_coffee.py

--- unsubsribe_coffee.py
+++ unsubsribe_coffee.py
@@ -26,34 +26,25 @@ mail.select("Inbox")
 typ, item = mail.search(None, 'SENTON', today)
 items = item[0].split()
 
-del_inds = []
-
 for emailid in items:
     typ, data = mail.fetch(emailid, '(RFC822)')  # gets the data from the email - RFC822 idicates ASCII
     email_body = data[0][1]
     msg = email.message_from_bytes(email_body)
     sender = email.utils.parseaddr(msg['From'])
-    subject = msg['Subject']
-    if subject != 'unsubscribe' and subject != 'subscribe':
-        continue
     sender = sender[-1]
+    subject = msg['Subject']
+    subject = subject.lower().strip()
     if subject == 'unsubscribe':
-        for i in range(0, len(email_list)):
-            if email_list[i]==sender:
-                del_inds.append(i)
-    else:
+        email_list.remove(sender)
+    elif subject == 'subscribe':
         if sender not in email_list:
             email_list.append(sender)
 
 mail.close()    # close email connection
 mail.logout()
-    
-for val in del_inds:
-    email_list.pop(val)
 
 f = open('emails.txt', 'w')
 for item in email_list:
     f.write(item)
     f.write('\n')
 f.close()
-
